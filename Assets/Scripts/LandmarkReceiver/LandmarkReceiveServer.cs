2adb839f0db94d619663a1ac3bc62513f4fcad56
EC-K <elxone.19jt@gmail.com>
2024-09-24 23:02:46 +0900

    feat: add landmark message receiver

---
+++ Assets/Scripts/LandmarkReceiver/LandmarkReceiveServer.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using HolisticPose;
+
+namespace VioletSolver.Network {
+    public class LandmarkReceiveServer : MonoBehaviour
+    {
+        UdpClient _udpClient;
+        [SerializeField] int _port = 9000;
+
+        readonly Action<SocketException> _SocketEcxeptionCallback;
+        readonly Action<ObjectDisposedException> _ObjectDisposedExceptionCallback;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _udpClient = new UdpClient(_port);
+            _udpClient.BeginReceive(OnReceived, _udpClient);
+        }
+
+        void OnReceived(IAsyncResult result)
+        {
+            UdpClient getUdp = (UdpClient)result.AsyncState;
+            IPEndPoint ipEnd = null;
+
+            try
+            {
+                byte[] getByte = getUdp.EndReceive(result, ref ipEnd);
+
+                var message = Landmark.Parser.ParseFrom(getByte);
+                //var message = Encoding.UTF8.GetString(getByte);
+                Debug.Log(message);
+            }
+            catch (SocketException e)
+            {
+                _SocketEcxeptionCallback(e);
+                return;
+            }catch (ObjectDisposedException e)
+            {
+                _ObjectDisposedExceptionCallback(e);
+                return;
+            }
+
+            _udpClient.BeginReceive(OnReceived, getUdp);
+        }
+
+        public void Dispose()
+        {
+            _udpClient?.Dispose();
+        }
+    }
+}
