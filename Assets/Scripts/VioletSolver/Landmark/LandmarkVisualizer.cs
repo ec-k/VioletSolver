439841b59fc5423107a672005804e8c8aeb86be0
EC-K <elxone.19jt@gmail.com>
2025-02-03 11:00:03 +0900

    feat: add hand landmark visualizer

--- Assets/Scripts/VioletSolver/Landmark/LandmarkVisualizer.cs
+++ Assets/Scripts/VioletSolver/Landmark/LandmarkVisualizer.cs
@@ -8,15 +8,30 @@ namespace VioletSolver.Landmarks
     {
         [SerializeField] GameObject src;
         LandmarkHandler _landmarkHandler;
-        List<GameObject> _targets;
+        List<GameObject> _poseTargets;
+        List<GameObject> _lHandTargets;
+        List<GameObject> _rHandTargets;
+
+        Vector3 _lHandPosOffset;
+        Vector3 _rHandPosOffset;
+
+        float _poseTargetSize = 0.05f;
+        float _handTargetSize = 0.01f;
 
         void Start()
         {
+            _lHandPosOffset = new(-0.5f, 0, 0);
+            _rHandPosOffset = new(0.5f, 0, 0);
+
             var initialCapacity = 30;
-            _targets = new GameObject[initialCapacity].ToList();
+            _poseTargets = new GameObject[initialCapacity].ToList();
+            _lHandTargets = new GameObject[initialCapacity].ToList();
+            _rHandTargets = new GameObject[initialCapacity].ToList();
             for (var i = 0; i < initialCapacity; i++)
             {
-                SetTarget(PrimitiveType.Sphere, i, ref _targets);
+                SetTarget(PrimitiveType.Sphere, i, _poseTargetSize, ref _poseTargets);
+                SetTarget(PrimitiveType.Sphere, i, _handTargetSize, ref _lHandTargets);
+                SetTarget(PrimitiveType.Sphere, i, _handTargetSize, ref _rHandTargets);
             }
 
             _landmarkHandler = src.GetComponent<AvatarAnimator>().Landmarks;
@@ -24,43 +39,68 @@ namespace VioletSolver.Landmarks
 
         void Update()
         {
-            var lm = _landmarkHandler.Landmarks.Pose;
-            if (lm == null)
-                return;
-            ExpandVisualsList(lm);
-            for (var i = 0; i < lm.Count; i++)
+            // Visualize Pose
             {
-                _targets[i].transform.position = lm.Landmarks[i].Position;
+                var lm = _landmarkHandler.Landmarks.Pose;
+                if (lm == null)
+                    return;
+                ExpandVisualsList(lm, _poseTargetSize);
+                for (var i = 0; i < lm.Count; i++)
+                {
+                    _poseTargets[i].transform.position = lm.Landmarks[i].Position;
+                }
+            }
+            // Visualize Left Hand
+            {
+                var lm = _landmarkHandler.Landmarks.LeftHand;
+                if (lm == null)
+                    return;
+                ExpandVisualsList(lm, _handTargetSize);
+                for (var i = 0; i < lm.Count; i++)
+                {
+                    _lHandTargets[i].transform.position = lm.Landmarks[i].Position + _lHandPosOffset;
+                }
+            }
+            // Visualize Right Hand
+            {
+                var lm = _landmarkHandler.Landmarks.RightHand;
+                if (lm == null)
+                    return;
+                ExpandVisualsList(lm, _handTargetSize);
+                for (var i = 0; i < lm.Count; i++)
+                {
+                    _rHandTargets[i].transform.position = lm.Landmarks[i].Position + _rHandPosOffset;
+                }
             }
         }
 
-        void ExpandVisualsList(ILandmarks lm)
+        void ExpandVisualsList(ILandmarks lm, float targetSize)
         {
             var landmarkCount = lm.Count;
-            var targetCount = _targets.Count;
+            var targetCount = _poseTargets.Count;
 
             if (landmarkCount < targetCount)
             {
                 for (var i = landmarkCount; i < targetCount; i++)
                 {
-                    Destroy(_targets[i].gameObject);
+                    Destroy(_poseTargets[i].gameObject);
                 }
-                _targets.RemoveRange(landmarkCount, targetCount - landmarkCount);
+                _poseTargets.RemoveRange(landmarkCount, targetCount - landmarkCount);
             }
             else
             {
-                CollectionUtils<GameObject>.ExpandList(lm.Count, ref _targets);
+                CollectionUtils<GameObject>.ExpandList(lm.Count, ref _poseTargets);
                 for (var i = targetCount; i < landmarkCount; i++)
                 {
-                    SetTarget(PrimitiveType.Sphere, i, ref _targets);
+                    SetTarget(PrimitiveType.Sphere, i, targetSize, ref _poseTargets);
                 }
             }
         }
 
-        void SetTarget(PrimitiveType shapeType, int index, ref List<GameObject> targets)
+        void SetTarget(PrimitiveType shapeType, int index, float targetSize, ref List<GameObject> targets)
         {
             var obj = GameObject.CreatePrimitive(shapeType);
-            obj.transform.localScale = Vector3.one * 0.1f;
+            obj.transform.localScale = Vector3.one * targetSize;
             obj.transform.SetParent(gameObject.transform);
             obj.name = index.ToString();
 
