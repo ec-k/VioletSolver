d7ce25fe67980ff57891cbc01523130c3fdc6199
EC-K <elxone.19jt@gmail.com>
2025-05-16 11:13:54 +0900

    feat(wip): adding motion overriding for typing motion generation

---
+++ Assets/VioletSolver/Runtime/LandmarkReceiver/PoseReceiver.cs
@@ -0,0 +1,31 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using uOSC;
+using UnityEngine;
+
+namespace VioletSolver.Network
+{
+    [RequireComponent(typeof(uOscServer))]
+    public class PoseReceiver : MonoBehaviour
+    {
+        [SerializeField] uOscServer _server;
+
+        public Dictionary<HumanBodyBones, Quaternion> Results { get; set; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Results = new();
+            _server.onDataReceived.AddListener(OnDataReceived);
+        }
+
+        void OnDataReceived(Message message)
+        {
+            var boneKey = (HumanBodyBones)Enum.Parse(typeof(HumanBodyBones), message.address);
+            var rotation = new Quaternion((float)message.values[0], (float)message.values[1], (float)message.values[2], (float)message.values[3]);
+            Results[boneKey] = rotation;
+        }
+    }
+}
