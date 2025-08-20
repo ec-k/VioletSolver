7b8cc8ce79300e83f31099f06cb04c7890dede58
EC-K <elxone.19jt@gmail.com>
2025-08-20 12:14:31 +0900

    chore: add recording management system in Hierarchy for developping

--- Assets/Development/Scripts/SolveAndRecord.cs
+++ Assets/Development/Scripts/SolveAndRecord.cs
@@ -1,24 +1,38 @@
 using UnityEngine;
 using VioletSolver.LandmarkProviders;
+using VioletSolver.Recorder;
 
 namespace VioletSolver.Development
 {
-    public class KeyboardPlaybackController : MonoBehaviour
+    public class SolveAndRecord : MonoBehaviour
     {
         [SerializeField] LandmarkPlaybackManager _playbackManager;
+        [SerializeField] RecorderController _motionRecorder;
         bool IsPlayingLog => _playbackManager.IsPlayingLog;
 
         void Start()
         {
-            if(_playbackManager == null)
+            if (_playbackManager == null)
             {
                 Debug.LogError("Playback Manager is not assigned in the inspector.");
                 enabled = false;
                 return;
             }
+            if(_motionRecorder == null)
+            {
+                Debug.LogError("Avatar Motion Recorder is not assigned in the inspector.");
+                enabled = false;
+                return;
+            }
+
+            _playbackManager.OnReachedToEnd += HandleOnReachedToEnd;
         }
 
-        // Update is called once per frame
+        void HandleOnReachedToEnd()
+        {
+            _motionRecorder.StopRecording();
+        }
+
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -26,15 +40,26 @@ namespace VioletSolver.Development
                 if (IsPlayingLog)
                 {
                     _playbackManager.PausePlayback();
+                    _motionRecorder.PauseRecording();
                 }
                 else
                 {
+                    _motionRecorder.StartRecording();
                     _playbackManager.StartPlayback();
                 }
             }
             if (Input.GetKeyDown(KeyCode.R))
             {
                 _playbackManager.ResetPlayback();
+                _motionRecorder.StopRecording();
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (_playbackManager != null)
+            {
+                _playbackManager.OnReachedToEnd -= HandleOnReachedToEnd;
             }
         }
     }
